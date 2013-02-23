b22fac8db63d274a3b09c07b330f9ccb90951788
ezgraphs <ezgraphs@gmail.com>
2013-02-23 11:41:25 -0500

    Update analyze_oo_html_doc.rb

--- analyze_oo_html_doc.rb
+++ analyze_oo_html_doc.rb
@@ -2,6 +2,7 @@
 #  When writing, it is helpful to review word usage to see if the author has gotten into a rut using the same words
 #  This program analyzes an HTML doc (exported from Open Office) and displays the word counts/locations of each word
 #
+#  Pipe to sort -n (or other utility) if further processing needed
 
 if ARGV[0].nil?
   puts "\n  Usage : #{__FILE__} <name of html file exported from Open Office>\n\n"
@@ -19,5 +20,6 @@ text.gsub!('          ',' ')                                             # etc.
 a=text.downcase.split                                                    # Downcase the letters
 h={}                                                                     # Create an empty hash
 a.each_with_index{|w,i| h[w].nil? ? h[w]=[i] :h[w]<<i }                  # For each word, save the word as key, and put its index into the value array
-h.keys.sort.each{|k|puts "#{h[k].size.to_s.ljust(10)} #{k.ljust(25)} #{h[k].join(', ')}"}  # Print out the number of occurences, key, and a list of indices
-# Pipe to sort -n if needed
+
+# Print out the number of occurences, key, and a list of indices
+h.keys.sort.each{|k|puts "#{h[k].size.to_s.ljust(10)} #{k.ljust(25)} #{h[k].join(', ')}"}  
